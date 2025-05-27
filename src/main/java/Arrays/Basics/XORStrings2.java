d8df4eab51bd58af3580bfb586e37b771d29c7b4
vivaan3757 <pralavsh07.ps@gmail.com>
2025-05-27 21:22:01 +0530

    Added XOR

--- src/main/java/Arrays/Basics/XORStrings2.java
+++ src/main/java/Arrays/Basics/XORStrings2.java
@@ -1,6 +1,28 @@
 package Arrays.Basics;
 
+import java.util.Scanner;
+
 public class XORStrings2 {
 
+    public static String strings_xor(String s, String t) {
+        String res = new String("");
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == t.charAt(i))
+                res += "0";
+            else
+                res += "1";
+        }
 
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String t = sc.nextLine();
+
+        System.out.println(strings_xor(s,t));
+    }
 }
